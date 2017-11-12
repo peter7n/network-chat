0e451e9d5a40861e67b99257ca0850f000a79411
Peter Nguyen <nguyenp2@oregonstate.edu>
2017-11-12 10:40:13 -0800

    cleanup & add final documentation

--- chatclient.c
+++ chatclient.c
@@ -15,37 +15,63 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
-// #include <arpa/inet.h>
 
 #define BUFFER_SIZE 500
 
+int serverConnect(char* userPort, char* userHost);
+void sendMessage(char* handle, char* clientMessage, char* prependMessage, int clientSocket);
+void receiveMessage(char* clientMessage, char* serverMessage, int clientSocket);
 void prependHandle(char* handle, char* message, char* prependMessage);
 
 int main(int argc, char *argv[])
 {
-   int serverPort = 0;
-   int clientSocket;
+   int clientSocket = 0;
    char handle[12];
    char prependMessage[512];
    char clientMessage[BUFFER_SIZE];
    char serverMessage[BUFFER_SIZE];
+
+   // Get user's handle name
+   printf("Enter your handle name: ");
+   fgets(handle, 12, stdin);
+   handle[strlen(handle) - 1] = '\0';   // remove newline
+
+   clientSocket = serverConnect(argv[2], argv[1]);
+
+   // Enter into main send/receive message loop
+   do
+   {
+      sendMessage(handle, clientMessage, prependMessage, clientSocket);
+      receiveMessage(clientMessage, serverMessage, clientSocket);
+   }
+   while (strcmp(clientMessage, "\\quit") != 0 &&
+         strstr(serverMessage, "\\quit") == NULL);
+
+   printf("CONNECTION CLOSED\n");
+   close(clientSocket);
+
+   return 0;
+}
+
+/*********************************************************************
+ ** Function: serverConnect
+ ** Description: Accepts client host name and port from the command
+ ** line; configures server address struct, connects to server;
+ ** Parameters: char* userPort, char* userHost
+ ** Returns: int, the new client socket
+ *********************************************************************/
+
+int serverConnect(char* userPort, char* userHost)
+{
+   int newSocket = 0;
+   int serverPort = 0;
    struct hostent *server;        // defines a host computer
    struct sockaddr_in serverAddress;
 
-   // Get user's handle name
-   do
-   {
-      printf("Enter your handle name: ");
-      fgets(handle, 12, stdin);
-      printf("%d\n", strlen(handle));
-      handle[strlen(handle) - 1] = '\0';   // remove newline
-   }
-   while (strlen(handle) > 10);
-
    // Set server name and port number
-   serverPort = atoi(argv[2]);
-   server = gethostbyname(argv[1]);
-   clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+   serverPort = atoi(userPort);
+   server = gethostbyname(userHost);
+   newSocket = socket(AF_INET, SOCK_STREAM, 0);
 
    // Configure server address struct
    bzero((char *) &serverAddress, sizeof(serverAddress));
@@ -56,36 +82,55 @@ int main(int argc, char *argv[])
    serverAddress.sin_port = htons(serverPort);
 
    // Connect to server
-   connect(clientSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
+   connect(newSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
 
-   do
-   {
-      // Get client message to send
-      printf("%s> ", handle);
-      fgets(clientMessage, BUFFER_SIZE, stdin);
-      clientMessage[strlen(clientMessage) - 1] = '\0';   // remove newline
-
-      // Send message with handle prepended to it
-      prependHandle(handle, clientMessage, prependMessage);
-      send(clientSocket, prependMessage, strlen(prependMessage), 0);
-
-      if (strcmp(clientMessage, "\\quit") != 0)
-      {
-         // Receive message from server
-         memset(serverMessage, 0, BUFFER_SIZE);   // clear serverMessage buffer
-         recv(clientSocket, serverMessage, BUFFER_SIZE, 0);
-         if (strstr(serverMessage, "\\quit") == NULL)
-            printf("%s\n", serverMessage);
-      }
-   }
-   while (strcmp(clientMessage, "\\quit") != 0 &&
-         strstr(serverMessage, "\\quit") == NULL);
-
-   printf("CONNECTION CLOSED\n");
-   close(clientSocket);
-
-   return 0;
+   return newSocket;
 }
+
+/*********************************************************************
+ ** Function: sendMessage
+ ** Description: Gets message input from user, prepends the user's
+ ** "handle" and sends message to the server
+ ** Parameters: char* handle, char* clientMessage, char* prependMessage, int clientSocket
+ *********************************************************************/
+
+void sendMessage(char* handle, char* clientMessage, char* prependMessage, int clientSocket)
+{
+   // Get input for the client message that will be sent
+   printf("%s> ", handle);
+   fgets(clientMessage, BUFFER_SIZE, stdin);
+   clientMessage[strlen(clientMessage) - 1] = '\0';   // remove newline
+
+   // Send message with handle prepended to it
+   prependHandle(handle, clientMessage, prependMessage);
+   send(clientSocket, prependMessage, strlen(prependMessage), 0);
+}
+
+/*********************************************************************
+ ** Function: receiveMessage
+ ** Description: Receives message from the server and prints it to the
+ ** screen if it does not contain "\quit"
+ ** Parameters: char* clientMessage, char* serverMessage, int clientSocket
+ *********************************************************************/
+
+void receiveMessage(char* clientMessage, char* serverMessage, int clientSocket)
+{
+   if (strcmp(clientMessage, "\\quit") != 0)
+   {
+      // Receive message from server
+      memset(serverMessage, 0, BUFFER_SIZE);   // clear serverMessage buffer
+      recv(clientSocket, serverMessage, BUFFER_SIZE, 0);
+      if (strstr(serverMessage, "\\quit") == NULL)
+         printf("%s\n", serverMessage);
+   }
+}
+
+/*********************************************************************
+ ** Function: prependHandle
+ ** Description: Takes a client message and prepends the user's
+ ** handle to the string
+ ** Parameters: char* handle, char* message, char* prependMessage
+ *********************************************************************/
 
 void prependHandle(char* handle, char* message, char* prependMessage)
 {
--- chatserve.py
+++ chatserve.py
@@ -1,36 +1,87 @@
-#  ==================================================================
-#  Program Filename: chatserve.py
-#  Author: Peter Nguyen
-#  Date: 10/29/17
-#  CS 372-400
-#  Description: Project 1 - Server application for chat that connects
-#  to client application using sockets API and TCP protocol.
-#  ==================================================================
+# ==================================================================
+# Program Filename: chatserve.py
+# Author: Peter Nguyen
+# Date: 10/29/17
+# CS 372-400
+# Description: Project 1 - Server application for chat that connects
+# to client application using sockets API and TCP protocol.
+# ==================================================================
 
 import sys
 from socket import *
 
-serverPort = int(sys.argv[1])
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('', serverPort))
+# ==================================================================
+# Function: serverSetup
+# Description: Takes a port number from the command line and binds
+# the port to a new socket
+# Parameters: userPort
+# Returns: newSocket
+#  ==================================================================
 
+def serverSetup(userPort):
+	# Get server port # from command line
+	serverPort = int(userPort)
+	newSocket = socket(AF_INET, SOCK_STREAM)
+	newSocket.bind(('', serverPort))
+	return newSocket
+
+# ==================================================================
+# Function: receiveMessage
+# Description: Receives message from client and prints it if the
+# message does not contain "\quit". Returns True if contains "\quit"
+# Parameters: conSocket
+# Returns: True or False
+#  ==================================================================
+
+def receiveMessage(conSocket):
+	clientMessage = conSocket.recv(1024).decode()
+	if "\\quit" in clientMessage:
+		print("CONNECTION CLOSED BY CLIENT")
+		return True
+	else:
+		print(clientMessage.decode())
+		return False
+
+# ==================================================================
+# Function: sendMessage
+# Description: Accepts user input for the message and sends it.
+# Returns True if the message contains "\quit"
+# Parameters: conSocket
+# Returns: True or False
+#  ==================================================================
+
+def sendMessage(conSocket):
+	serverMessage = raw_input('server> ')
+	serverMessage = "server> " + serverMessage
+	conSocket.send(serverMessage.encode())
+	if "\\quit" in serverMessage:
+		return True
+	else:
+		return False
+
+# ==================================================================
+# Main Program
+# ==================================================================
+
+# Initialize "welcome" socket
+serverSocket = serverSetup(sys.argv[1])
+
+# Continue to listen for new clients until SIGINT received
 while True:
 	serverSocket.listen(1)
 	print("Server is ready to receive")
 
 	connectionSocket, address = serverSocket.accept()
 
+	# Continue to send/receive messages to/from current client
+	# while messages don't contain "\quit"
 	while True:
-		clientMessage = connectionSocket.recv(1024).decode()
-		if "\\quit" in clientMessage:
-			print("CONNECTION CLOSED BY CLIENT")
+		quitFlag = receiveMessage(connectionSocket)
+		if quitFlag:
 			break
 		else:
-			print(clientMessage.decode())
-			serverMessage = raw_input('server> ')
-			serverMessage = "server> " + serverMessage
-			connectionSocket.send(serverMessage.encode())
-		if "\\quit" in serverMessage:
+			quitFlag = sendMessage(connectionSocket)
+		if quitFlag:
 			break
 
 	connectionSocket.close()
